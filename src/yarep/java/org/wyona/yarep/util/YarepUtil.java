666d315d6e357a4191a8d488bd6bf1bef2034505
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2006-06-12 22:31:46 +0000

    yarep util started

---
+++ src/yarep/java/org/wyona/yarep/util/YarepUtil.java
@@ -0,0 +1,51 @@
+package org.wyona.yarep.util;
+
+import org.apache.log4j.Category;
+
+import org.wyona.yarep.core.Path;
+import org.wyona.yarep.core.Repository;
+import org.wyona.yarep.core.RepositoryFactory;
+
+/**
+ *
+ */
+public class YarepUtil {
+
+    private static Category log = Category.getInstance(YarepUtil.class);
+
+    /**
+     *
+     */
+    public static Repository getRepositoryId(Path path, RepositoryFactory repoFactory) {
+        Repository repo = null;
+
+        // Determine possible Repository ID. If such a repo ID doesn't exist, then use ROOT repository
+	String[] splittedPath = path.toString().split("/");
+        if (splittedPath != null) {
+            if (splittedPath.length < 2) {
+	        log.debug("Length = " + splittedPath.length + ". Use ROOT repository.");
+            } else {
+                if (repoFactory.exists(splittedPath[1])) {
+                    repo = repoFactory.newRepository(splittedPath[1]);
+                    log.debug("New Repository: " + repo.getID() + " - " + repo.getName());
+
+                    log.debug("Repo ID length: " + repo.getID().length());
+                    path = new Path(path.toString().substring(repo.getID().length() + 1));
+                    log.debug("New Path: " + path);
+                    return repo;
+                } else {
+                    log.debug("No such repository \"" + splittedPath[1] + "\". Use ROOT repository.");
+                }
+            }
+        } else {
+            log.debug("Path could not be split. Use ROOT repository.");
+        }
+
+        // First repository shall be ROOT repository
+        repo = repoFactory.firstRepository();
+        log.debug("ROOT Repository: " + repo.getID() + " - " + repo.getName());
+
+        log.debug("Path (still original): " + path);
+        return repo;
+    }
+}
