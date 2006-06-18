ddf806e32e632c80eb9038422ee4466a2055ed7b
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2006-06-18 10:49:07 +0000

    add tools and build.sh

---
+++ build.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+echo "INFO: Build Yarep ..."
+
+# ----- Parameters
+
+# ----- Check for JAVA_HOME
+JAVA_HOME="$JAVA_HOME"
+if [ "$JAVA_HOME" = "" ];then
+  echo "ERROR: No JAVA_HOME set!"
+  echo "       Have you installed JDK (Java Development Kit)? If so, then set JAVA_HOME ..."
+  echo "       MacOS X : setenv JAVA_HOME /usr"
+  echo "       Linux   : export JAVA_HOME=/usr/local/j2sdk-..."
+  echo "       Windows : Click Start ..."
+  exit 1
+fi
+
+# ----- Check Java version
+# TODO: ....
+
+# ----- Set Environment Variables
+ORIGINAL_ANT_HOME=$ANT_HOME
+unset ANT_HOME
+ANT_HOME=$PWD/tools/apache-ant-1.6.2
+#echo $ANT_HOME
+
+ORIGINAL_PATH=$PATH
+PATH=$PWD/tools/maven-2.0.4/bin:$ANT_HOME/bin:$PATH
+#echo $PATH
+
+# ----- Build Yarep ...
+#mvn --version
+ant -version
+ant -f build.xml $@
+
+# ----- Reset Environment Variables
+ANT_HOME=$ORIGINAL_ANT_HOME
+#echo $ANT_HOME
+PATH=$ORIGINAL_PATH
+#echo $PATH
