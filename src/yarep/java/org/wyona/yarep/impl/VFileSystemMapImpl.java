8e847184bc19bdc42512dd3b732439fb8bd84b6e
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2006-07-25 13:48:28 +0000

    VFS map added

---
+++ src/yarep/java/org/wyona/yarep/impl/VFileSystemMapImpl.java
@@ -0,0 +1,103 @@
+package org.wyona.yarep.impl;
+
+import org.apache.avalon.framework.configuration.Configuration;
+
+import org.wyona.commons.io.FileUtil;
+import org.wyona.yarep.core.Map;
+import org.wyona.yarep.core.Path;
+import org.wyona.yarep.core.UID;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.BufferedReader;
+
+import org.apache.log4j.Category;
+
+/**
+ *
+ */
+public class VFileSystemMapImpl implements Map {
+
+    private static Category log = Category.getInstance(VFileSystemMapImpl.class);
+
+    protected File pathsDir;
+
+    /**
+     *
+     */
+    public void readConfig(Configuration mapConfig, File repoConfigFile) {
+        try {
+            pathsDir = new File(mapConfig.getAttribute("src"));
+            if (!pathsDir.isAbsolute()) {
+                pathsDir = FileUtil.file(repoConfigFile.getParent(), pathsDir.toString());
+            }
+            log.debug(pathsDir.toString());
+            // TODO: Throw Exception
+            if (!pathsDir.exists()) log.error("No such file or directory: " + pathsDir);
+        } catch(Exception e) {
+            log.error(e);
+        }
+    }
+
+    /**
+     *
+     */
+    public boolean isResource(Path path) {
+        File file = new File(pathsDir + path.toString());
+        return file.isFile();
+    }
+
+    /**
+     *
+     */
+    public boolean exists(Path path) {
+        File file = new File(pathsDir + path.toString());
+        // TODO: Get name of repository for debugging ...
+        //log.debug("Path (" + getName() + "): " + file);
+        return file.exists();
+    }
+
+    /**
+     *
+     */
+    public boolean isCollection(Path path) {
+        File file = new File(pathsDir + path.toString());
+        return file.isDirectory();
+    }
+
+    /**
+     *
+     */
+    public Path[] getChildren(Path path) {
+        File file = new File(pathsDir + path.toString());
+        String[] filenames = file.list();
+
+	// NOTE: This situation should only occur if isResource(Path) didn't work properly!
+        if (filenames == null) {
+            log.warn("No children: " + path + " (" + file + ")");
+            return new Path[0];
+        }
+
+        log.debug("Number of children: " + filenames.length + " (" + file + ")");
+        Path[] children = new Path[filenames.length];
+        for (int i = 0;i < children.length; i++) {
+            if (path.toString().endsWith(File.separator)) {
+                children[i] = new Path(path + filenames[i]);
+            } else {
+                // NOTE: Do not use File.separator here, because it's the repository path and not the Operating System File System path
+                children[i] = new Path(path + "/" + filenames[i]);
+            }
+            log.debug("Child: " + children[i]);
+        }
+        return children;
+    }
+
+    /**
+     * Get UID
+     */
+    public synchronized UID getUID(Path path) {
+        // TODO: Check if leading slash should be removed ...
+        return new UID(path.toString());
+    }
+}
