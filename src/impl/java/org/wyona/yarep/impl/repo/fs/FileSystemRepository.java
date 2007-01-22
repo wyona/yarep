ed0f29c41e841f03d21606634c8209259c728f1b
josias <josias@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2007-01-22 16:29:29 +0000

    removed deprecated functionality. only node-based functionality is supported now in this class

--- src/impl/java/org/wyona/yarep/impl/repo/fs/FileSystemRepository.java
+++ src/impl/java/org/wyona/yarep/impl/repo/fs/FileSystemRepository.java
@@ -1,25 +1,14 @@
 package org.wyona.yarep.impl.repo.fs;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.io.Reader;
-import java.io.StringWriter;
-import java.io.UnsupportedEncodingException;
 import java.io.Writer;
 
 import org.apache.avalon.framework.configuration.Configuration;
 import org.apache.avalon.framework.configuration.DefaultConfigurationBuilder;
-
 import org.apache.log4j.Category;
-
 import org.wyona.commons.io.FileUtil;
 import org.wyona.yarep.core.Map;
 import org.wyona.yarep.core.NoSuchNodeException;
@@ -31,9 +20,9 @@ import org.wyona.yarep.core.Storage;
 import org.wyona.yarep.core.UID;
 
 /**
- *
+ * Node based file system repository.
  */
-public class FileSystemRepository  implements Repository {
+public class FileSystemRepository implements Repository {
 
     private static Category log = Category.getInstance(FileSystemRepository.class);
 
@@ -101,256 +90,106 @@ public class FileSystemRepository  implements Repository {
         }
     }
 
-    /**
-     *
-     */
-    public String toString() {
-        return "Repository: ID = " + id + ", Configuration-File = " + configFile + ", Name = " + name;
+    public void addSymbolicLink(Path target, Path link) throws RepositoryException {
+        log.warn("Not implemented.");
     }
 
-    /**
-     * Get repository ID
-     */
-    public String getID() {
-        return id;
-    }
-
-    /**
-     * Set repository ID
-     */
-    public void setID(String id) {
-        this.id = id;
-    }
-    
-    /**
-     * Get repository name
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * Get repository configuration file
-     */
-    public File getConfigFile() {
-        return configFile;
-    }
-
-    /**
-     *
-     */
-    public Writer getWriter(Path path) throws RepositoryException {
-        OutputStream out = getOutputStream(path);
-        try {
-            if (out != null) {
-                return new OutputStreamWriter(getOutputStream(path), "UTF-8");
-            } else {
-                return null;
-            }
-        } catch (UnsupportedEncodingException e) {
-            throw new RepositoryException("Could not read path: " + path + ": " + e.getMessage(), e);
-        }
-    }
-
-    /**
-     *
-     */
-    public OutputStream getOutputStream(Path path) throws RepositoryException  {
-        UID uid = getUID(path);
-        if (uid == null) {
-            if (fallback) {
-                log.warn("No path to get UID from! Fallback to : " + path);
-                uid = new UID(path.toString());
-                map.addSymbolicLink(path, uid);
-            } else {
-                uid = map.create(path);
-            }
-        }
-        log.debug(uid.toString());
-        return storage.getOutputStream(uid, path);
-    }
-
-    /**
-     *
-     */
-    public Reader getReader(Path path) throws RepositoryException {
-        try {
-            return new InputStreamReader(getInputStream(path), "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            throw new RepositoryException("Could not read path: " + path + ": " + e.getMessage(), e);
-        }
-    }
-
-    /**
-     *
-     */
-    public InputStream getInputStream(Path path) throws RepositoryException {
-        UID uid = null;
-        if (!exists(path)) {
-            if (fallback) {
-                log.info("No UID! Fallback to : " + path);
-                uid = new UID(path.toString());
-            } else {
-                throw new NoSuchNodeException(path, this);
-            }
-        } else {
-            uid = getUID(path);
-        }
-        if (uid == null) {
-            log.error("No UID: " + path);
-            return null;
-        }
-        log.debug(uid.toString());
-        return storage.getInputStream(uid, path);
-    }
-
-    /**
-     *
-     */
-    public long getLastModified(Path path) throws RepositoryException {
-        UID uid = getUID(path);
-        if (uid == null) {
-            log.error("No UID: " + path);
-            return -1;
-        }
-        return storage.getLastModified(uid, path);
-    }
-    
-    /**
-     *
-     */
-    public long getSize(Path path) throws RepositoryException {
-    	UID uid = getUID(path);
-    	if (uid == null) {
-    		log.error("No UID: " + path);
-    		return -1;
-    	}
-    	return storage.getSize(uid, path);
-    }
-    
-    /**
-     * @return true if node has been deleted, otherwise false
-     */
     public boolean delete(Path path) throws RepositoryException {
-        if(map.isCollection(path)) {
-            log.warn("Node is a collection and hence cannot be deleted: " + path);
-            return false;
-        }
-        UID uid = getUID(path);
-        if (uid == null) {
-            if (fallback) {
-                log.warn("Fallback: " + path);
-                return storage.delete(new UID(path.toString()), path);
-            } else {
-                log.error("No UID: " + path);
-                return false;
-            }
-        }
-        return map.delete(path) && storage.delete(uid, path);
+        log.warn("Not implemented.");
+        return false;
     }
 
-    /**
-     * Not implemented yet
-     * http://excalibur.apache.org/apidocs/org/apache/excalibur/source/impl/FileSource.html#getValidity()
-     * http://excalibur.apache.org/apidocs/org/apache/excalibur/source/SourceValidity.html
-     */
-    public void getValidity(Path path) throws RepositoryException {
-        log.error("TODO: No implemented yet!");
-    }
-
-    /**
-     * Not implemented yet
-     * http://excalibur.apache.org/apidocs/org/apache/excalibur/source/impl/FileSource.html#getContentLength()
-     */
-    public void getContentLength(Path path) throws RepositoryException {
-        log.error("TODO: No implemented yet!");
-    }
-
-    /**
-     * Not implemented yet
-     * http://excalibur.apache.org/apidocs/org/apache/excalibur/source/impl/FileSource.html#getURI()
-     */
-    public void getURI(Path path) throws RepositoryException {
-        log.error("TODO: No implemented yet!");
-    }
-
-    /**
-     *
-     */
-    public boolean isResource(Path path) throws RepositoryException {
-        return map.isResource(path);
-    }
-
-    /**
-     * One might want to discuss what is a collection. A resource for instance could
-     * also be a collection, but a collection with some default content.
-     * In the case of JCR there are only nodes and properties!
-     */
-    public boolean isCollection(Path path) throws RepositoryException {
-        return map.isCollection(path);
-    }
-
-    /**
-     *
-     */
     public boolean exists(Path path) throws RepositoryException {
-       return map.exists(path);
+        log.warn("Not implemented.");
+        return false;
     }
 
-    /**
-     *
-     */
     public Path[] getChildren(Path path) throws RepositoryException {
-        if (fallback) {
-            log.warn("Repository " + getName() + " has fallback enabled and hence some children might be missed because these only exist within the storage (Path: " + path + ")");
-        }
-        // TODO: Order by last modified resp. alphabetical resp. ...
-        return map.getChildren(path);
+        log.warn("Not implemented.");
+        return null;
     }
 
-    /**
-     * Get UID
-     *
-     * http://www.ietf.org/rfc/rfc4122.txt
-     * http://incubator.apache.org/jackrabbit/apidocs/org/apache/jackrabbit/uuid/UUID.html
-     * http://www.webdav.org/specs/draft-leach-uuids-guids-01.txt
-     */
-    public synchronized UID getUID(Path path) throws RepositoryException {
-        return map.getUID(path);
+    public File getConfigFile() {
+        log.warn("Not implemented.");
+        return null;
     }
-    
-    /**
-     * Get all revision numbers of the given path.
-     * @return Array of revision number strings. Newest revision first. 
-     */
-   public String[] getRevisions(Path path) throws RepositoryException {
-       UID uid = getUID(path);
-       //if (uid == null) throw new NoSuchNodeException("Path not found: " + path);
-       // fallback?
-       return storage.getRevisions(uid, path);
-   }
 
-    /**
-     * Add symbolic link
-     */
-    public void addSymbolicLink(Path target, Path link) throws NoSuchNodeException, RepositoryException {
-        log.debug("Target: " + target);
-        UID uid = null;
-        if (!exists(target)) {
-            if (fallback) {
-                log.warn("No UID! Fallback to : " + target);
-                uid = new UID(target.toString());
-            } else {
-                throw new NoSuchNodeException(target, this);
-            }
-        } else {
-            uid = getUID(target);
-        }
-        log.debug("UID of Target: " + uid);
-        log.debug("Link: " + link);
-        map.addSymbolicLink(link, uid);
+    public void getContentLength(Path path) throws RepositoryException {
+        log.warn("Not implemented.");
     }
-    
+
+    public String getID() {
+        log.warn("Not implemented.");
+        return null;
+    }
+
+    public InputStream getInputStream(Path path) throws RepositoryException {
+        log.warn("Not implemented.");
+        return null;
+    }
+
+    public long getLastModified(Path path) throws RepositoryException {
+        log.warn("Not implemented.");
+        return 0;
+    }
+
+    public String getName() {
+        log.warn("Not implemented.");
+        return null;
+    }
+
+    public OutputStream getOutputStream(Path path) throws RepositoryException {
+        log.warn("Not implemented.");
+        return null;
+    }
+
+    public Reader getReader(Path path) throws RepositoryException {
+        log.warn("Not implemented.");
+        return null;
+    }
+
+    public String[] getRevisions(Path path) throws RepositoryException {
+        log.warn("Not implemented.");
+        return null;
+    }
+
+    public long getSize(Path path) throws RepositoryException {
+        log.warn("Not implemented.");
+        return 0;
+    }
+
+    public UID getUID(Path path) throws RepositoryException {
+        log.warn("Not implemented.");
+        return null;
+    }
+
+    public void getURI(Path path) throws RepositoryException {
+        log.warn("Not implemented.");
+    }
+
+    public void getValidity(Path path) throws RepositoryException {
+        log.warn("Not implemented.");
+    }
+
+    public Writer getWriter(Path path) throws RepositoryException {
+        log.warn("Not implemented.");
+        return null;
+    }
+
+    public boolean isCollection(Path path) throws RepositoryException {
+        log.warn("Not implemented.");
+        return false;
+    }
+
+    public boolean isResource(Path path) throws RepositoryException {
+        log.warn("Not implemented.");
+        return false;
+    }
+
+    public void setID(String id) {
+        log.warn("Not implemented.");
+    }
+
     ///////////////////////////////////////////////////////////////////////////
     // New methods for node based repository
     ///////////////////////////////////////////////////////////////////////////
