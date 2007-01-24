3ddc7ae9c769171cde0f0d39ad6e2dd3f9da4123
josias <josias@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2007-01-24 10:13:42 +0000

    deprecated old methods

--- src/core/java/org/wyona/yarep/core/Repository.java
+++ src/core/java/org/wyona/yarep/core/Repository.java
@@ -37,58 +37,62 @@ public interface Repository {
     public File getConfigFile();
 
     /**
-     *
+     * @deprecated
      */
     public Writer getWriter(Path path) throws RepositoryException;
 
     /**
-     *
+     * @deprecated
      */
     public OutputStream getOutputStream(Path path) throws RepositoryException;
 
     /**
-     *
+     * @deprecated
      */
     public Reader getReader(Path path) throws RepositoryException;
 
     /**
-     *
+     * @deprecated
      */
     public InputStream getInputStream(Path path) throws RepositoryException;
 
     /**
-     *
+     * @deprecated
      */
     public long getLastModified(Path path) throws RepositoryException;
     
     /**
-     *
+     * @deprecated
      */
     public long getSize(Path path) throws RepositoryException;
     
     /**
      * @return true if node has been deleted, otherwise false
+     * @deprecated
      */
     public boolean delete(Path path) throws RepositoryException;
 
     /**
      * http://excalibur.apache.org/apidocs/org/apache/excalibur/source/impl/FileSource.html#getValidity()
      * http://excalibur.apache.org/apidocs/org/apache/excalibur/source/SourceValidity.html
+     * @deprecated
      */
     public void getValidity(Path path) throws RepositoryException;
 
     /**
      * http://excalibur.apache.org/apidocs/org/apache/excalibur/source/impl/FileSource.html#getContentLength()
+     * @deprecated
      */
     public void getContentLength(Path path) throws RepositoryException;
 
     /**
      * http://excalibur.apache.org/apidocs/org/apache/excalibur/source/impl/FileSource.html#getURI()
+     * @deprecated
      */
     public void getURI(Path path) throws RepositoryException;
 
     /**
-     *
+     * @deprecated
      */
     public boolean isResource(Path path) throws RepositoryException;
 
@@ -96,16 +100,17 @@ public interface Repository {
      * One might want to discuss what is a collection. A resource for instance could
      * also be a collection, but a collection with some default content.
      * In the case of JCR there are only nodes and properties!
+     * @deprecated
      */
     public boolean isCollection(Path path) throws RepositoryException;
 
     /**
-     *
+     * @deprecated
      */
     public boolean exists(Path path) throws RepositoryException;
 
     /**
-     *
+     * @deprecated
      */
     public Path[] getChildren(Path path) throws RepositoryException;
 
@@ -115,17 +120,20 @@ public interface Repository {
      * http://www.ietf.org/rfc/rfc4122.txt
      * http://incubator.apache.org/jackrabbit/apidocs/org/apache/jackrabbit/uuid/UUID.html
      * http://www.webdav.org/specs/draft-leach-uuids-guids-01.txt
+     * @deprecated
      */
     public UID getUID(Path path) throws RepositoryException;
     
     /**
      * Get all revision numbers of the given path.
      * @return Array of revision number strings. Newest revision first. 
+     * @deprecated
      */
    public String[] getRevisions(Path path) throws RepositoryException;
 
     /**
      * Add symbolic link
+     * @deprecated
      */
     public void addSymbolicLink(Path target, Path link) throws RepositoryException;
     
