375a03fde4e086c2a2be5e76311d94ccd2397cc4
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2007-05-30 09:55:34 +0000

    new virtual fs repo implementation started

---
+++ src/impl/java/org/wyona/yarep/impl/repo/vfs/VirtualFileSystemOutputStream.java
@@ -0,0 +1,58 @@
+package org.wyona.yarep.impl.repo.vfs;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.apache.log4j.Category;
+import org.wyona.yarep.core.Node;
+import org.wyona.yarep.core.RepositoryException;
+import org.wyona.yarep.impl.AbstractNode;
+
+/**
+ * OutputStream which sets some properties (lastModified, size) to the node 
+ * when the stream is closed.
+ * 
+ * NOTE: Currently not used, because the Node implemenation uses the lastModified and size
+ * of the content file.
+ */
+public class VirtualFileSystemOutputStream extends OutputStream {
+
+    private static Category log = Category.getInstance(VirtualFileSystemOutputStream.class);
+
+    protected OutputStream out;
+    protected Node node;
+    protected File file;
+
+    /**
+     * 
+     */
+    public VirtualFileSystemOutputStream(Node node, File file) throws FileNotFoundException {
+        this.node  = node;
+        this.out = new FileOutputStream(file);
+        this.file = file;
+    }
+    
+    /**
+     * 
+     */
+    public void write(int b) throws IOException {
+        out.write(b);
+    }
+
+    /**
+     * 
+     */
+    public void close() throws IOException {
+        out.close();
+        try {
+            node.setProperty(AbstractNode.PROPERTY_SIZE, file.length());
+            node.setProperty(AbstractNode.PROPERTY_LAST_MODIFIED, file.lastModified());
+        } catch (RepositoryException e) {
+            log.error(e.getMessage(), e);
+            throw new IOException(e.getMessage());
+        }
+    }
+}
---
+++ src/impl/java/org/wyona/yarep/impl/repo/vfs/VirtualFileSystemRevision.java
@@ -0,0 +1,167 @@
+package org.wyona.yarep.impl.repo.vfs;
+
+import java.io.File;
+import java.util.Date;
+
+import org.apache.log4j.Category;
+import org.wyona.yarep.core.NoSuchRevisionException;
+import org.wyona.yarep.core.Node;
+import org.wyona.yarep.core.NodeStateException;
+import org.wyona.yarep.core.Property;
+import org.wyona.yarep.core.RepositoryException;
+import org.wyona.yarep.core.Revision;
+
+/**
+ */
+public class VirtualFileSystemRevision extends VirtualFileSystemNode implements Revision {
+
+    private static Category log = Category.getInstance(VirtualFileSystemRevision.class);
+    
+    public static final String PROPERTY_REVISION_CREATION_DATE = "yarep_revisionCreationDate";
+    public static final String PROPERTY_REVISION_CREATOR = "yarep_revisionCreator";
+    public static final String PROPERTY_REVISION_TAG = "yarep_revisionTag";
+    public static final String PROPERTY_REVISION_COMMENT = "yarep_revisionComment";
+
+    public static final String CONTENT_FILE_NAME = "content";
+
+    protected Node node;
+    protected String revisionName;
+    
+    /**
+     * Constructor
+     * @throws RepositoryException
+     */
+    public VirtualFileSystemRevision(VirtualFileSystemNode node, String revisionName) throws RepositoryException {
+        super(node.getRepository(), node.getPath(), node.getUUID(), false);
+
+        this.revisionName = revisionName;
+        this.contentDir = new File(((VirtualFileSystemRepository)repository).getContentDir(), this.uuid + META_DIR_SUFFIX + File.separator + REVISIONS_BASE_DIR + File.separator + this.revisionName);
+        this.contentFile = new File(this.contentDir, CONTENT_FILE_NAME);
+        this.metaDir = this.contentDir;
+        this.metaFile = new File(this.metaDir, META_FILE_NAME);
+    
+        if (log.isDebugEnabled()) {
+            log.debug("VirtualFileSystemRevision: path=" + path + " uuid=" + uuid + " revisionName=" + revisionName);
+            log.debug("contentDir=" + contentDir);
+            log.debug("contentFile=" + contentFile);
+            log.debug("metaDir=" + metaDir);
+            log.debug("metaFile=" + metaFile);
+        }
+
+        if (!metaFile.exists()) {
+            throw new RepositoryException("Meta file " + metaFile + " does not exist.");
+        }
+        readProperties();
+    }
+    
+    /**
+     * @see org.wyona.yarep.impl.repo.fs.VirtualFileSystemNode#addNode(java.lang.String, int)
+     */
+    public Node addNode(String name, int type) throws RepositoryException {
+        throw new RepositoryException("cannot call this method on a revision");
+    }
+    
+    /**
+     * @see org.wyona.yarep.impl.repo.fs.VirtualFileSystemNode#checkin()
+     */
+    public Revision checkin() throws NodeStateException, RepositoryException {
+        throw new RepositoryException("cannot call this method on a revision");
+    }
+    
+    /**
+     * @see org.wyona.yarep.impl.repo.fs.VirtualFileSystemNode#checkout(java.lang.String)
+     */
+    public void checkout(String userID) throws NodeStateException, RepositoryException {
+        throw new RepositoryException("cannot call this method on a revision");
+    }
+    
+    protected Revision createRevision() throws RepositoryException {
+        throw new RepositoryException("cannot call this method on a revision");
+    }
+    
+    /**
+     * @see org.wyona.yarep.impl.repo.fs.VirtualFileSystemNode#restore(java.lang.String)
+     */
+    public void restore(String revisionName) throws NoSuchRevisionException, RepositoryException {
+        throw new RepositoryException("cannot call this method on a revision");
+    }
+    
+    /**
+     * @see org.wyona.yarep.core.Revision#getCreationDate()
+     */
+    public Date getCreationDate() throws RepositoryException {
+        return getProperty(PROPERTY_REVISION_CREATION_DATE).getDate();
+    }
+
+    /**
+     * Sets the creation date of this revision.
+     * @param date
+     * @throws RepositoryException
+     */
+    public void setCreationDate(Date date) throws RepositoryException {
+        setProperty(PROPERTY_REVISION_CREATION_DATE, date);
+    }
+
+    /**
+     * @see org.wyona.yarep.core.Revision#getCreator()
+     */
+    public String getCreator() throws RepositoryException {
+        return getProperty(PROPERTY_REVISION_CREATOR).getString();
+    }
+
+    /**
+     * Sets the creator of this revision.
+     * @param creator user id
+     * @throws RepositoryException
+     */
+    public void setCreator(String creator) throws RepositoryException {
+        setProperty(PROPERTY_REVISION_CREATOR, creator);
+    }
+
+    /**
+     * @see org.wyona.yarep.core.Revision#getComment()
+     */
+    public String getComment() throws RepositoryException {
+        return getProperty(PROPERTY_REVISION_COMMENT).getString();
+    }
+
+    /**
+     * Sets the comment about this revision.
+     * @param comment
+     * @throws RepositoryException
+     */
+    public void setComment(String comment) throws RepositoryException {
+        setProperty(PROPERTY_REVISION_COMMENT, comment);
+    }
+
+    /**
+     * @see org.wyona.yarep.core.Revision#getTag()
+     */
+    public String getTag() throws RepositoryException {
+        Property tag = getProperty(PROPERTY_REVISION_TAG);
+        if (tag == null) return null;
+        return tag.getString();
+    }
+
+    /**
+     * @see org.wyona.yarep.core.Revision#setTag(java.lang.String)
+     */
+    public void setTag(String tag) throws RepositoryException {
+        setProperty(PROPERTY_REVISION_TAG, tag);
+    }    
+       
+    /**
+     * @see org.wyona.yarep.core.Revision#hasTag()
+     */
+    public boolean hasTag() throws RepositoryException {
+        return hasProperty(PROPERTY_REVISION_TAG);
+    }    
+       
+    /**
+     * @see org.wyona.yarep.core.Revision#getRevisionName()
+     */
+    public String getRevisionName() throws RepositoryException {
+        return this.revisionName;
+    }
+
+}
