ee567275df1e4e54f779b46fea717f9d54116e6d
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2007-11-14 11:17:17 +0000

    JCR repo impl started

---
+++ src/impl/java/org/wyona/yarep/impl/repo/jcr/JCRRepository.java
@@ -0,0 +1,257 @@
+package org.wyona.yarep.impl.repo.jcr;
+
+import org.wyona.yarep.core.Node;
+import org.wyona.yarep.core.NoSuchNodeException;
+import org.wyona.yarep.core.Path;
+import org.wyona.yarep.core.Repository;
+import org.wyona.yarep.core.RepositoryException;
+import org.wyona.yarep.core.UID;
+
+import org.apache.log4j.Category;
+
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
+
+/**
+ * JCR based repository.
+ */
+public class JCRRepository implements Repository {
+
+    private static Category log = Category.getInstance(JCRRepository.class);
+
+    private String repoId;
+
+    /**
+     *
+     */
+    public JCRRepository() {
+    }
+
+    /**
+     *
+     */
+    public void move(String srcPath, String destPath) throws RepositoryException {
+        log.error("Not implemented yet!");
+    }
+    
+    /**
+     *
+     */
+    public void copy(String srcPath, String destPath) throws RepositoryException {
+        log.error("Not implemented yet!");
+    }
+
+    /**
+     *
+     */
+    public Node getRootNode() throws RepositoryException {
+        log.error("Not implemented yet!");
+        return null;
+    }
+
+    /**
+     *
+     */
+    public boolean existsNode(String path) throws RepositoryException {
+        log.error("Not implemented yet!");
+        return true;
+    }
+
+    /**
+     *
+     */
+    public Node getNodeByUUID(String uuid) throws NoSuchNodeException, RepositoryException {
+        log.error("Not implemented yet!");
+        return null;
+    }
+
+    /**
+     *
+     */
+    public Node getNode(String path) throws NoSuchNodeException, RepositoryException {
+        log.error("Not implemented yet!");
+        return null;
+    }
+   
+    /**
+     *
+     */
+    public void addSymbolicLink(Path target, Path link) throws RepositoryException {
+        log.error("Not implemented yet!");
+    }
+
+    /**
+     *
+     */
+    public String[] getRevisions(Path path) throws RepositoryException {
+        log.error("Not implemented yet!");
+        return null;
+    }
+
+    /**
+     *
+     */
+    public UID getUID(Path path) throws RepositoryException {
+        log.error("Not implemented yet!");
+        return null;
+    }
+
+    /**
+     *
+     */
+    public Path[] getChildren(Path path) throws RepositoryException {
+        log.error("Not implemented yet!");
+        return null;
+    }
+
+    /**
+     *
+     */
+    public boolean exists(Path path) throws RepositoryException {
+        return existsNode(path.toString());
+    }
+
+    /**
+     *
+     */
+    public boolean isCollection(Path path) throws RepositoryException {
+        return getNode(path.toString()).isCollection();
+    }
+
+    /**
+     *
+     */
+    public boolean isResource(Path path) throws RepositoryException {
+        return getNode(path.toString()).isResource();
+    }
+
+    /**
+     *
+     */
+    public void getURI(Path path) throws RepositoryException {
+        log.error("Not implemented yet!");
+    }
+
+    /**
+     *
+     */
+    public void getContentLength(Path path) throws RepositoryException {
+        log.error("Not implemented yet!");
+    }
+
+    /**
+     *
+     */
+    public void getValidity(Path path) throws RepositoryException {
+        log.error("Not implemented yet!");
+    }
+
+    /**
+     *
+     */
+    public boolean delete(Path path) throws RepositoryException {
+        getNode(path.toString()).delete();
+        return true;
+    }
+
+    /**
+     *
+     */
+    public boolean delete(Path path, boolean recursive) throws RepositoryException {
+        if (recursive) throw new RepositoryException("Not implemented yet!");
+        return delete(path);
+    }
+
+    /**
+     *
+     */
+    public long getSize(Path path) throws RepositoryException {
+        return getNode(path.toString()).getSize();
+    }
+
+    /**
+     *
+     */
+    public long getLastModified(Path path) throws RepositoryException {
+        return getNode(path.toString()).getLastModified();
+    }
+
+    /**
+     *
+     */
+    public InputStream getInputStream(Path path) throws RepositoryException {
+        return getNode(path.toString()).getInputStream();
+    }
+
+    /**
+     *
+     */
+    public OutputStream getOutputStream(Path path) throws RepositoryException {
+        return getNode(path.toString()).getOutputStream();
+    }
+
+    /**
+     *
+     */
+    public Reader getReader(Path path) throws RepositoryException {
+        try {
+            return new InputStreamReader(getNode(path.toString()).getInputStream(), "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RepositoryException(e.getMessage(), e);
+        }
+    }
+
+    /**
+     *
+     */
+    public Writer getWriter(Path path) throws RepositoryException {
+        try {
+            return new OutputStreamWriter(getNode(path.toString()).getOutputStream(), "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RepositoryException(e.getMessage(), e);
+        }
+    }
+
+    /**
+     *
+     */
+    public File getConfigFile() {
+        log.error("Not implemented yet!");
+        return null;
+    }
+
+    /**
+     *
+     */
+    public String getName() {
+        log.error("Not implemented yet!");
+        return null;
+    }
+
+    /**
+     *
+     */
+    public void readConfiguration(File configFile) throws RepositoryException {
+        log.error("Not implemented yet!");
+    }
+
+    /**
+     *
+     */
+    public String getID() {
+        return repoId;
+    }
+
+    /**
+     *
+     */
+    public void setID(String id) {
+        repoId = id;
+    }
+}
