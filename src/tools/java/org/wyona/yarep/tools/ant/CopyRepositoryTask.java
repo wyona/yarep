903ff7efb03bb97b8803dbc99cf8b2b3b7c8472c
josias <josias@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2007-11-20 15:48:20 +0000

    improved logging

--- src/tools/java/org/wyona/yarep/tools/ant/CopyRepositoryTask.java
+++ src/tools/java/org/wyona/yarep/tools/ant/CopyRepositoryTask.java
@@ -3,16 +3,20 @@ package org.wyona.yarep.tools.ant;
 
 import java.io.File;
 
+import org.apache.log4j.Logger;
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
 import org.apache.tools.ant.types.Path;
 import org.wyona.yarep.core.Repository;
 import org.wyona.yarep.core.RepositoryException;
 import org.wyona.yarep.core.RepositoryFactory;
+import org.wyona.yarep.impl.repo.jcr.JCRRepository;
 import org.wyona.yarep.util.YarepUtil;
 
 
 public class CopyRepositoryTask extends Task {
+    private static Logger log = Logger.getLogger(JCRRepository.class);
+    
     private Path srcRepoConfigPath;
     private Path destRepoConfigPath;
     private String srcRepoID;
@@ -23,26 +27,41 @@ public class CopyRepositoryTask extends Task {
      */
     public void execute() throws BuildException {
         try {
-            System.out.println("src repo id: " + getSrcRepoID());
-            System.out.println("src repo config path: " + getSrcRepoConfigPath());
-            System.out.println("dest repo id: " + getDestRepoID());
-            System.out.println("dest repo config path: " + getDestRepoConfigPath());
+            log.info("src repo id: " + getSrcRepoID());
+            log.info("src repo config path: " + getSrcRepoConfigPath());
+            log.info("dest repo id: " + getDestRepoID());
+            log.info("dest repo config path: " + getDestRepoConfigPath());
             
             RepositoryFactory repoFactory = new RepositoryFactory();
             
+            String[] ids = repoFactory.getRepositoryIDs();
+            if (ids.length == 0) {
+                throw new BuildException("no repositories found!");
+            }
+            for (int i = 0; i < ids.length; i++) {
+                log.info("repository: " + ids[i]);
+            }
             //File srcRepoConfigFile = new File(getSrcRepoConfigPath().toString());
             Repository srcRepo = repoFactory.newRepository(getSrcRepoID());
             //Repository srcRepo = repoFactory.newRepository(getSrcRepoID(), srcRepoConfigFile);
+            if (srcRepo == null) {
+                throw new BuildException("src repo [" + getSrcRepoID() + "] is null!");
+            }
             
             //File destRepoConfigFile = new File(getDestRepoConfigPath().toString());
             Repository destRepo = repoFactory.newRepository(getDestRepoID());
             //Repository destRepo = repoFactory.newRepository(getDestRepoID(), destRepoConfigFile);
+            if (destRepo == null) {
+                throw new BuildException("dest repo [" + getDestRepoID() + "] is null!");
+            }
             
-            System.out.println("starting to copy...");
+            
+            log.info("starting to copy...");
             YarepUtil.copyRepository(srcRepo, destRepo);
-            System.out.println("done.");
+            log.info("done.");
             
-        } catch (RepositoryException e) {
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
             throw new BuildException(e.getMessage(), e);
         }
 
