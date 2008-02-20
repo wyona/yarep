6fafd4e48d8513567de38f89865b5347dba51c92
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2008-02-20 13:06:44 +0000

    logger introduced

--- src/test/java/org/wyona/yarep/tests/VirtualFilesystemRepoTest.java
+++ src/test/java/org/wyona/yarep/tests/VirtualFilesystemRepoTest.java
@@ -4,14 +4,19 @@ import java.io.File;
 
 import org.wyona.yarep.core.RepositoryFactory;
 
+import org.apache.log4j.Logger;
+
 /**
  * Test for the 'virtual filesystem' repository implementation.
  */
 public class VirtualFilesystemRepoTest extends NodeRepoTest {
 
+    private static Logger log = Logger.getLogger(VirtualFilesystemRepoTest.class);
+
     public void setUp() throws Exception {
         RepositoryFactory repoFactory = new RepositoryFactory();
         repo = repoFactory.newRepository("vfs-example", new File("new-vfs-example/repository.xml"));
+        log.info("Testing repository: " + repo.getName());
     }
     
     protected String getCollectionTestPath() {
