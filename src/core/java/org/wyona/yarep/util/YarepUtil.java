8a4d5939bef2012c3c0d7db3a44cfd0b85904d02
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2008-04-15 20:57:21 +0000

    addNodes added

--- src/core/java/org/wyona/yarep/util/YarepUtil.java
+++ src/core/java/org/wyona/yarep/util/YarepUtil.java
@@ -104,4 +104,26 @@ public class YarepUtil {
             log.error("Could not copy node: " + srcNode.getPath() + ": " + e.getMessage(), e);
         }
     }
+
+    /**
+     * Creates the node named by this abstract pathname, including any necessary but nonexistent parent nodes (similar to java.io.File.mkdirs()).
+     */
+    public static Node addNodes(Repository repo, String path, int nodeType) throws RepositoryException {
+        if (repo.existsNode(path)) {
+            return repo.getNode(path);
+        } else {
+            org.wyona.commons.io.Path parentPath = new org.wyona.commons.io.Path(path).getParent();
+            if (parentPath != null) {
+                Node parentNode = null;
+                if (repo.existsNode(parentPath.toString())) {
+                    parentNode = repo.getNode(parentPath.toString());
+                } else {
+                    parentNode = addNodes(repo, parentPath.toString(), org.wyona.yarep.core.NodeType.COLLECTION);
+                }
+                return parentNode.addNode(new Path(path).getName().toString(), nodeType);
+            } else {
+                throw new RepositoryException("Root node does not have a parent!");
+            }
+        }
+    }
 }
