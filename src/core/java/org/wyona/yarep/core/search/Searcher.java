2ce5cec8e0ac1169f66504b4134c97f80b7be22e
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2008-09-11 08:18:57 +0000

    searcher added

---
+++ src/core/java/org/wyona/yarep/core/search/Searcher.java
@@ -0,0 +1,38 @@
+package org.wyona.yarep.core.search;
+
+import org.apache.avalon.framework.configuration.Configuration;
+import org.wyona.yarep.core.Node;
+import org.wyona.yarep.core.Repository;
+
+import java.io.File;
+
+/**
+ *  Search Interface
+ */
+public interface Searcher {
+
+    /**
+     * Configure searcher
+     *
+     * @param searchIndexConfig The part of the yarep repository configuration containing the search/index configuration
+     * @param configFile The file where the yarep repository configuration is located (useful when resolving relative paths of referenced files). TODO: Maybe replace later with a resolver!
+     * @param repo Yarep Repository which contains content related to index which is searched in
+     */
+    public void configure(Configuration searchIndexConfig, File configFile, Repository repo) throws SearchException;
+    
+    /**
+     * Search content
+     *
+     * @param query Search query
+     */
+    public Node[] search(String query) throws SearchException;
+    
+    /**
+     * Search for properties content within subtree
+     *
+     * @param pName Parameter name
+     * @param pValue Parameter value of search query
+     * @param path Scope of search (path of subtree)
+     */
+    public Node[] searchProperty(String pName, String pValue, String path) throws SearchException;
+}
