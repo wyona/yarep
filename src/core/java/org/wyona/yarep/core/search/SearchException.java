994610967fe278b1ecf0a9ca6aa1e8d444360157
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2008-09-11 08:02:20 +0000

    search exception added

---
+++ src/core/java/org/wyona/yarep/core/search/SearchException.java
@@ -0,0 +1,35 @@
+package org.wyona.yarep.core.search;
+
+/**
+ *
+ */
+public class SearchException extends Exception {
+
+    /**
+     *
+     */
+    public SearchException() {
+        super();
+    }
+
+    /**
+     *
+     */
+    public SearchException(Throwable t) {
+        super(t);
+    }
+
+    /**
+     *
+     */
+    public SearchException(String s) {
+        super(s);
+    }
+
+    /**
+     *
+     */
+    public SearchException(String s, Throwable t) {
+        super(s, t);
+    }
+}
