1225ebb380bcfe04c133a7ac292c49087ac84e69
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2009-12-30 09:56:25 +0000

    creating a single revision completed

--- src/test/java/org/wyona/yarep/tests/VirtualFilesystemRevisionsTest.java
+++ src/test/java/org/wyona/yarep/tests/VirtualFilesystemRevisionsTest.java
@@ -4,6 +4,7 @@ import org.wyona.yarep.core.Node;
 import org.wyona.yarep.core.NodeType;
 import org.wyona.yarep.core.Repository;
 import org.wyona.yarep.core.RepositoryFactory;
+import org.wyona.yarep.core.Revision;
 
 import org.apache.log4j.Logger;
 
@@ -23,7 +24,7 @@ public class VirtualFilesystemRevisionsTest extends TestCase {
     public void setUp() throws Exception {
         RepositoryFactory repoFactory = new RepositoryFactory();
         repo = repoFactory.newRepository("vfs-example", new java.io.File("new-vfs-example/repository.xml"));
-        log.info("Testing repository: " + repo.getName());
+        log.info("Testing repository: " + repo.getName() + " (" + repo.getConfigFile() + ")");
     }
 
     /**
@@ -36,28 +37,14 @@ public class VirtualFilesystemRevisionsTest extends TestCase {
         }
 
         Node node = repo.getRootNode().addNode(NODE_NAME, NodeType.RESOURCE);
+        node.checkout("bob");
+        node.setMimeType("text/plain");
         java.io.PrintWriter pw = new java.io.PrintWriter(node.getOutputStream());
         pw.print("Hello revision test!");
         pw.close();
+        Revision revision = node.checkin("My first revision");
 
-        assertTrue("Revision has been created: ", true);
+        log.info("Revision has been created: " + revision.getRevisionName());
+        assertTrue("Revision has been created: " + revision.getRevisionName(), revision != null);
     }
-
-/*
-    protected String getCollectionTestPath() {
-        return "/another-directory";
-    }
-    
-    protected String getResourceTestName() {
-        return "index.html";
-    }
-    
-    protected String getResourceTestPath() {
-        return getCollectionTestPath() + "/" + getResourceTestName();
-    }
-    
-    protected String getRevisionTestPath() {
-        return "/hello-world.txt";
-    }
-*/
 }
