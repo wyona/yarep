37f3828197b97c36cd9a491c645e04c864f72578
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2010-11-10 08:41:49 +0000

    get nodes added

--- src/test/java/org/wyona/yarep/tests/VirtualFilesystemRepoTest.java
+++ src/test/java/org/wyona/yarep/tests/VirtualFilesystemRepoTest.java
@@ -6,6 +6,8 @@ import org.wyona.yarep.core.RepositoryFactory;
 
 import org.apache.log4j.Logger;
 
+import org.wyona.yarep.core.Node;
+
 /**
  * Test for the 'virtual filesystem' repository implementation.
  */
@@ -13,27 +15,39 @@ public class VirtualFilesystemRepoTest extends NodeRepoTest {
 
     private static Logger log = Logger.getLogger(VirtualFilesystemRepoTest.class);
 
+    @Override
     public void setUp() throws Exception {
         RepositoryFactory repoFactory = new RepositoryFactory();
         repo = repoFactory.newRepository("vfs-example", new File("new-vfs-example/repository.xml"));
         log.info("Testing repository: " + repo.getName());
     }
-    
+
+    @Override
     protected String getCollectionTestPath() {
         return "/another-directory";
     }
     
+    @Override
     protected String getResourceTestName() {
         return "index.html";
     }
     
+    @Override
     protected String getResourceTestPath() {
         return getCollectionTestPath() + "/" + getResourceTestName();
     }
     
+    @Override
     protected String getRevisionTestPath() {
         return "/hello-world.txt";
     }
 
-    
+    /**
+     * Test get children of a node
+     */
+    public void testGetNodes() throws Exception {
+        Node node = repo.getNode(getCollectionTestPath());
+        node.getNodes();
+        assertEquals(node.getNodes().length, 1);
+    }
 }
