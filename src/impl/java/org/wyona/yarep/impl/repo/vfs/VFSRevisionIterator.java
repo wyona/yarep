e1b2e5d7d499ecea0452fe25e1b78640085b22dd
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2011-04-07 08:38:29 +0000

    point in time added

--- src/impl/java/org/wyona/yarep/impl/repo/vfs/VFSRevisionIterator.java
+++ src/impl/java/org/wyona/yarep/impl/repo/vfs/VFSRevisionIterator.java
@@ -4,6 +4,7 @@ import org.wyona.yarep.core.Node;
 import org.wyona.yarep.core.Revision;
 
 import java.io.File;
+import java.util.Date;
 
 import org.apache.log4j.Logger;
 
@@ -15,14 +16,18 @@ public class VFSRevisionIterator implements java.util.Iterator {
     private static Logger log = Logger.getLogger(VFSRevisionIterator.class);
 
     private Revision currentRevision;
+
     private DateIndexerSearcher dis;
+    private Date pointInTime;
 
     /**
      * @param node Yarep node which is supposed to have revisions
      * @param metaDir Meta directory containing revisions index
+     * @param date Point in time (from where iteration shall start or stop)
      */
-    public VFSRevisionIterator(Node node, File metaDir, boolean reverse) throws Exception {
-        dis = new DateIndexerSearcher(node, metaDir);
+    public VFSRevisionIterator(Node node, File metaDir, Date date, boolean reverse) throws Exception {
+        this.pointInTime = date;
+        this.dis = new DateIndexerSearcher(node, metaDir);
         if (!dis.indexExists()) {
             dis.buildDateIndex();
         }
@@ -34,15 +39,28 @@ public class VFSRevisionIterator implements java.util.Iterator {
      */
     public boolean hasNext() {
         if (currentRevision == null) {
-            if (dis.getMostRecentRevision() != null) {
-                return true;
+            if (pointInTime != null) {
+                try {
+                    if (dis.getRevision(pointInTime) != null) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                } catch(Exception e) {
+                    log.error(e, e);
+                    return false;
+                }
             } else {
-                return false;
+                if (dis.getMostRecentRevision() != null) {
+                    return true;
+                } else {
+                    return false;
+                }
             }
         } else {
             try {
-                java.util.Date currentDate = currentRevision.getCreationDate();
-                if (dis.getRevisionOlderThan(currentDate) != null) {
+                log.debug("Check if there is another revision older than: " + format(currentRevision.getCreationDate()));
+                if (dis.getRevisionOlderThan(currentRevision.getCreationDate()) != null) {
                     return true;
                 } else {
                     return false;
@@ -59,12 +77,20 @@ public class VFSRevisionIterator implements java.util.Iterator {
      */
     public Object next() throws java.util.NoSuchElementException {
         if (currentRevision == null) {
-            currentRevision = dis.getMostRecentRevision();
+            if (pointInTime != null) {
+                try {
+                    currentRevision = dis.getRevision(pointInTime);
+                } catch(Exception e) {
+                    throw new java.util.NoSuchElementException(e.getMessage());
+                }
+            } else {
+                currentRevision = dis.getMostRecentRevision();
+            }
             return currentRevision;
         } else {
             try {
-                java.util.Date currentDate = currentRevision.getCreationDate();
-                currentRevision = dis.getRevisionOlderThan(currentDate);
+                log.debug("Get revision older than: " + format(currentRevision.getCreationDate()));
+                currentRevision = dis.getRevisionOlderThan(currentRevision.getCreationDate());
                 if (currentRevision != null) {
                     return currentRevision;
                 } else {
@@ -81,5 +107,12 @@ public class VFSRevisionIterator implements java.util.Iterator {
      */
     public void remove() throws UnsupportedOperationException {
         throw new UnsupportedOperationException("Not implemented.");
+    }
+
+    /**
+     * Format date
+     */
+    private String format(java.util.Date date) {
+        return new java.text.SimpleDateFormat("yyyy.MM.dd'T'HH:mm:ss/SZ").format(date);
     }
 }
