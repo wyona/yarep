747948930d9076668dc039893dce5a70af930177
Michael Wechner <michael.wechner@wyona.com>
2011-12-15 15:39:01 +0100

    javadoc added, logging improved, get missing nodes implemented

--- src/impl/java/org/wyona/yarep/impl/search/lucene/LuceneSearcher.java
+++ src/impl/java/org/wyona/yarep/impl/search/lucene/LuceneSearcher.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.avalon.framework.configuration.Configuration;
+
 import org.apache.log4j.Logger;
+
 import org.apache.lucene.search.IndexSearcher;
+
 import org.wyona.yarep.core.NoSuchNodeException;
 import org.wyona.yarep.core.Node;
 import org.wyona.yarep.core.Repository;
@@ -20,14 +23,22 @@ import java.io.File;
 public class LuceneSearcher implements Searcher {
     
     static Logger log = Logger.getLogger(LuceneSearcher.class);
+
     private LuceneConfig config;
-    
+
+    private static final String PATH_FIELD_NAME = "_PATH";
+
+    private boolean autoClean = false;
+  
+    /**
+     * @see org.wyona.yarep.core.search.Searcher#configure(Configuration, File, Repository)
+     */  
     public void configure(Configuration searchIndexConfig, File configFile, Repository repo) throws SearchException {
         this.config = new LuceneConfig(searchIndexConfig, configFile.getParent(), repo);
     }
     
     /**
-     * Search content
+     * @see org.wyona.yarep.core.search.Searcher#search(String)
      */
     public Node[] search(String query) throws SearchException {
         try {
@@ -43,7 +54,7 @@ public class LuceneSearcher implements Searcher {
 
                     java.util.List<Node> results = new java.util.ArrayList<Node>();
                     for (int i = 0; i < hits.length();i++) {
-                        String path = hits.doc(i).getField("_PATH").stringValue();
+                        String path = hits.doc(i).getField(PATH_FIELD_NAME).stringValue();
                         if (path.contains("#revision=")) {
                             //log.debug("This seems to be a revision: " + resultPath);
                             String resultPathWithoutRevision = path.substring(0, path.lastIndexOf("#revision="));
@@ -101,13 +112,17 @@ public class LuceneSearcher implements Searcher {
                     org.apache.lucene.search.Query luceneQuery = queryParser.parse(query);
 
                     org.apache.lucene.search.Hits hits = searcher.search(luceneQuery);
-                    log.info("Number of matching documents: " + hits.length());
+                    log.info("Number of matching documents (Property: " + pName + ", Query: " + query + ", Path: " + path + "): " + hits.length());
                     List results = new ArrayList();
                     for (int i = 0; i < hits.length(); i++) {
                         try {
-                            String resultPath = hits.doc(i).getField("_PATH").stringValue();
-                            
+                            String resultPath = hits.doc(i).getField(PATH_FIELD_NAME).stringValue();
+
                             // subtree filter (WARN: Peformance/Scalability!)
+                            if (path == null) {
+                                path = "/";
+                                log.warn("No scope path set, hence set it ROOT: " + path);
+                            }
                             if (resultPath.startsWith(path)) {
                                 if (resultPath.contains("#revision=")) {
                                     //log.debug("This seems to be a revision: " + resultPath);
@@ -123,12 +138,23 @@ public class LuceneSearcher implements Searcher {
                                         log.error("Node found within search index, but no such node within repository: " + resultPathWithoutRevision);
                                     }
                                 } else {
-                                    results.add(config.getRepo().getNode(resultPath));
+                                    if (config.getRepo().existsNode(resultPath)) {
+                                        results.add(config.getRepo().getNode(resultPath));
+                                    } else {
+                                        log.error("Node found within search index, but no such node within repository: " + resultPath);
+                                        if(autoClean) {
+                                            // TODO: Remove entry from index
+                                        }
+                                    }
                                 }
+                            } else {
+                                log.warn("Scope path '" + path + "' did not match result path: " + resultPath);
                             }
-
-                        } catch (NoSuchNodeException nsne) {
-                            log.warn("Node found within search index, but no such node within repository: " + hits.doc(i).getField("_PATH").stringValue());
+                        } catch (NoSuchNodeException nsne) { // INFO: I think catching this exception is not really necessary anymore. because the code above already checks the existence...
+                            log.warn("Node found within search index, but no such node within repository: " + hits.doc(i).getField(PATH_FIELD_NAME).stringValue());
+                            if(autoClean) {
+                                // TODO: Remove entry from index
+                            }
                         }
                     }
                     searcher.close();
@@ -144,5 +170,67 @@ public class LuceneSearcher implements Searcher {
             throw new SearchException(e.getMessage(),e);
         }
         return null;
+    }
+
+    /**
+     * Get list of paths of of nodes and/or revisions, which do not exist anymore inside repository
+     * @return List of paths of of nodes and/or revisions, which do not exist anymore inside repository
+     */
+    public String[] getMissingNodes() throws SearchException {
+        try {
+            org.apache.lucene.search.Searcher searcher = new IndexSearcher(config.getPropertiesSearchIndexFile().getAbsolutePath());
+            if (searcher != null) {
+                try {
+                    org.apache.lucene.search.Query luceneQuery = new org.apache.lucene.search.MatchAllDocsQuery();
+
+                    org.apache.lucene.search.Hits hits = searcher.search(luceneQuery);
+                    log.warn("DEBUG: Number of matching documents: " + hits.length());
+                    log.info("Number of matching documents: " + hits.length());
+
+                    List<String> results = new ArrayList<String>();
+                    for (int i = 0; i < hits.length(); i++) {
+                        String resultPath = hits.doc(i).getField(PATH_FIELD_NAME).stringValue();
+                        try {
+                            if (resultPath.contains("#revision=")) {
+                                //log.debug("This seems to be a revision: " + resultPath);
+                                String resultPathWithoutRevision = resultPath.substring(0, resultPath.lastIndexOf("#revision="));
+                                String revisionName = resultPath.substring(resultPath.lastIndexOf("#revision=") + 10);
+                                if (config.getRepo().existsNode(resultPathWithoutRevision)) {
+                                    try {
+                                        config.getRepo().getNode(resultPathWithoutRevision).getRevision(revisionName);
+                                    } catch(org.wyona.yarep.core.NoSuchRevisionException e) {
+                                        log.error("Revision found within search index and node itself exists inside repository, but no such revision within repository: " + resultPathWithoutRevision + "#" + revisionName);
+                                        results.add(resultPath);
+                                    }
+                                } else {
+                                    log.error("Revision '" + resultPath + "' found within search index, but no such node within repository: " + resultPathWithoutRevision);
+                                    results.add(resultPathWithoutRevision);
+                                }
+                            } else {
+                                if (!config.getRepo().existsNode(resultPath)) {
+                                    log.error("Node found within search index, but no such node within repository: " + resultPath);
+                                    results.add(resultPath);
+                                }
+                            }
+                        } catch (NoSuchNodeException nsne) { // INFO: I think catching this exception is not really necessary anymore. because the code above already checks the existence...
+                            log.warn("Node found within search index, but no such node within repository: " + resultPath);
+                            results.add(resultPath);
+                        }
+                    }
+                    searcher.close();
+
+                    return (String[])results.toArray(new String[results.size()]);
+                } catch (Exception e) {
+                    log.error(e, e);
+                    throw new SearchException(e.getMessage(),e);
+                }
+            } else {
+                log.error("Searcher is null!");
+                return null;
+            }
+        } catch (Exception e) {
+            log.error(e, e);
+            throw new SearchException(e.getMessage(),e);
+        }
     }
 }
