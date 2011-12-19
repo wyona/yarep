6815fb055724b8d8efdf3ea41716b2a927e52765
Michael Wechner <michael.wechner@wyona.com>
2011-12-19 13:52:33 +0100

    delete flag and number of nodes introduced

--- src/tools/java/org/wyona/yarep/tools/cmdl/CleanIndex.java
+++ src/tools/java/org/wyona/yarep/tools/cmdl/CleanIndex.java
@@ -4,6 +4,7 @@ import org.wyona.yarep.core.Node;
 import org.wyona.yarep.core.Repository;
 import org.wyona.yarep.core.RepositoryFactory;
 import org.wyona.yarep.core.Revision;
+import org.wyona.yarep.impl.search.lucene.LuceneSearcher;
 
 import java.io.File;
 
@@ -21,8 +22,8 @@ public class CleanIndex {
      */
     public static void main(String[] args) {
 
-        if(args.length != 1) {
-            System.out.println("Usage: <data respository configuration>");
+        if(args.length != 3) {
+            System.out.println("Usage: <data respository configuration> <boolean delete flag> <number of nodes to be deleted>");
             return;
         }
 
@@ -40,6 +41,13 @@ public class CleanIndex {
         Repository repo;
         try {
             repo = repoFactory.newRepository("my-repo", new File(args[0]));
+            LuceneSearcher luceneSearcher = (LuceneSearcher) repo.getSearcher();
+            boolean delete = new Boolean(args[1]).booleanValue();
+            int listSizeLimit = new Integer(args[2]).intValue();
+            String[] missingNodePaths = luceneSearcher.getMissingNodes(delete, listSizeLimit);
+            for (int i = 0; i < missingNodePaths.length; i++) {
+                System.out.println("Missing node: " + missingNodePaths[i]);
+            }
         } catch (Exception e) {
             System.err.println(e);
             return;
