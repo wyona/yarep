a2f3e6b85269edd8bb6a0af53e5252656d47b060
Michael Wechner <michael.wechner@wyona.com>
2014-01-17 12:16:34 +0100

    instructions about JAVA_HOME improved

--- build.sh
+++ build.sh
@@ -4,12 +4,18 @@ echo "INFO: Build Yarep ..."
 
 # ----- Parameters
 
+JAVA_HOME_MACOSX=/System/Library/Frameworks/JavaVM.framework/Home
+
 # ----- Check for JAVA_HOME
 JAVA_HOME="$JAVA_HOME"
 if [ "$JAVA_HOME" = "" ];then
   echo "ERROR: No JAVA_HOME set!"
   echo "       Have you installed JDK (Java Development Kit)? If so, then set JAVA_HOME ..."
-  echo "       MacOS X : setenv JAVA_HOME /System/Library/Frameworks/JavaVM.framework/Home"
+  echo ""
+  echo "       Mac OS X : Depending on the shell you're using either use"
+  echo "                  export JAVA_HOME=$JAVA_HOME_MACOSX"
+  echo "                  or"
+  echo "                  setenv JAVA_HOME $JAVA_HOME_MACOSX"
   echo "       Linux   : export JAVA_HOME=/usr/local/j2sdk-..."
   echo "       Windows : Click Start ..."
   exit 1
