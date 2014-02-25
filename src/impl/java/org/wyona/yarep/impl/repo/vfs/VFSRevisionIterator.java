841b9ce206b31fedd2f041dfd80502c8a4577d96
Michael Wechner <michael.wechner@wyona.com>
2014-02-25 20:59:13 +0100

    reverse implemented for case without point in time

--- src/impl/java/org/wyona/yarep/impl/repo/vfs/VFSRevisionIterator.java
+++ src/impl/java/org/wyona/yarep/impl/repo/vfs/VFSRevisionIterator.java
@@ -25,15 +25,15 @@ public class VFSRevisionIterator implements java.util.Iterator {
     /**
      * @param dis Yarep node specific date indexer searche whuch is supposed to have revisions
      * @param date Point in time (from where iteration shall start or stop)
-     * @param reverse TODO
+     * @param reverse Reversed order, if set to true, then oldest revision first and most recent (head) last
      */
     public VFSRevisionIterator(DateIndexerSearcher dis, Date date, boolean reverse) throws Exception {
         this.pointInTime = date;
         this.dis = dis;
 
         this.reverse = reverse;
-        if (reverse) {
-            log.warn("TODO: Reverse order feature not implemented yet!");
+        if (reverse && pointInTime != null) {
+            log.warn("TODO: Finish reverse order implementation when using point in time!");
         }
 
         if (!dis.indexExists()) {
@@ -60,22 +60,39 @@ public class VFSRevisionIterator implements java.util.Iterator {
                     return false;
                 }
             } else {
-                if (reverse && dis.getOldestRevision() != null) {
-                    return true;
-                } else if (dis.getMostRecentRevision() != null) {
-                    return true;
+                if (reverse) {
+                    if (dis.getOldestRevision() != null) {
+                        return true;
+                    } else {
+                        log.warn("No revisions at all.");
+                        return false;
+                    }
                 } else {
-                    return false;
+                    if (dis.getMostRecentRevision() != null) {
+                        return true;
+                    } else {
+                        log.warn("No revisions at all.");
+                        return false;
+                    }
                 }
             }
         } else {
             try {
-                // TODO: Implement 'reverse'
                 log.debug("Check if there is another revision older than: " + format(currentRevision.getCreationDate()));
-                if (dis.getRevisionOlderThan(currentRevision.getCreationDate()) != null) {
-                    return true;
+                if (reverse) {
+                    if (dis.getRevisionYoungerThan(currentRevision.getCreationDate()) != null) {
+                        return true;
+                    } else {
+                        log.debug("No revision younger than '" + currentRevision.getCreationDate() + "', hence stop iteration.");
+                        return false;
+                    }
                 } else {
-                    return false;
+                    if (dis.getRevisionOlderThan(currentRevision.getCreationDate()) != null) {
+                        return true;
+                    } else {
+                        log.debug("No revision older than '" + currentRevision.getCreationDate() + "', hence stop iteration.");
+                        return false;
+                   }
                 }
             } catch(Exception e) {
                 log.error(e, e);
@@ -90,6 +107,7 @@ public class VFSRevisionIterator implements java.util.Iterator {
     public Object next() throws java.util.NoSuchElementException {
         if (currentRevision == null) {
             if (pointInTime != null) {
+                // TODO: Finish reverse implementation
                 try {
                     currentRevision = dis.getRevision(pointInTime);
                     //log.debug("First revision found '" + format(currentRevision.getCreationDate()) + "' for point in time: " + format(pointInTime));
@@ -97,13 +115,22 @@ public class VFSRevisionIterator implements java.util.Iterator {
                     throw new java.util.NoSuchElementException(e.getMessage());
                 }
             } else {
-                currentRevision = dis.getMostRecentRevision();
+                if (reverse) {
+                    currentRevision = dis.getOldestRevision();
+                } else {
+                    currentRevision = dis.getMostRecentRevision();
+                }
             }
             return currentRevision;
         } else {
             try {
-                log.debug("Get revision older than: " + format(currentRevision.getCreationDate()));
-                currentRevision = dis.getRevisionOlderThan(currentRevision.getCreationDate());
+                if (reverse) {
+                    log.debug("Get revision younger than: " + format(currentRevision.getCreationDate()));
+                    currentRevision = dis.getRevisionYoungerThan(currentRevision.getCreationDate());
+                } else {
+                    log.debug("Get revision older than: " + format(currentRevision.getCreationDate()));
+                    currentRevision = dis.getRevisionOlderThan(currentRevision.getCreationDate());
+                }
                 if (currentRevision != null) {
                     return currentRevision;
                 } else {
